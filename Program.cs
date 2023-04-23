176a1b324e50293b7056a1c0a3d7b6ab4670155c
oksana avdonina <oxana_2007@mail.ru>
2023-04-23 08:00:32 +0400

    1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] CreateArray(int length)
+{
+    string[] arr = new string[length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите слово: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+
